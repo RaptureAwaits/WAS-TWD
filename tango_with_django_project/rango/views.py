adce80cb009b00c5bd53d1441c83dcaa9e91b81e
RaptureAwaits <fredericsmythe512@gmail.com>
2022-04-09 22:25:06 +0100

    Implemented session cookies

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,8 +4,33 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+
+    if not val:
+        val = default_val
+
+    return val
 
 
 def index(request):
@@ -17,11 +42,16 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
-    return render(request, 'rango/about.html', context={})
+    visitor_cookie_handler(request)
+    context = {'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context=context)
 
 
 def show_category(request, category_name_slug):
